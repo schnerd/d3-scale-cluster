a87e42672a58ce3631c43d9341d7f332c730343c
David Schnurr <schnurr@gmail.com>
2016-09-14 19:55:55 +0200

    Initial Commit

---
+++ spec/index.js
@@ -0,0 +1,25 @@
+var Jasmine = require('jasmine');
+var JasmineConsoleReporter = require('jasmine-console-reporter');
+
+var jasmine = new Jasmine();
+jasmine.loadConfig({
+	spec_dir: 'spec',
+	spec_files: [
+		'**/*[sS]pec.js'
+	],
+	helpers: [
+		'helpers/**/*.js'
+	],
+	stopSpecOnExpectationFailure: false,
+	random: false
+});
+
+jasmine.addReporter(new JasmineConsoleReporter({
+	colors: 1,           // (0|false)|(1|true)|2 
+	cleanStack: 1,       // (0|false)|(1|true)|2|3 
+	verbosity: 4,        // (0|false)|1|2|(3|true)|4 
+	listStyle: 'indent', // "flat"|"indent" 
+	activity: false
+}));
+
+jasmine.execute();
---
+++ src/index.js
@@ -0,0 +1,80 @@
+var ckmeans = require('./ckmeans.js');
+
+module.exports = function d3scaleCluster () {
+  var clusters = [];
+  var domain = [];
+  var range = [];
+  var breakpoints = [];
+
+  var scale = function (x) {
+    if (clusters.length === 0) return undefined;
+
+    for (var i = breakpoints.length - 1; i >= 0; i--) {
+      if (x >= breakpoints[i]) {
+        return range[i];
+      }
+    }
+    return range[0];
+  };
+
+  function rescale () {
+    if (range.length <= 2) {
+      return;
+    }
+    clusters = ckmeans(domain, range.length);
+
+    breakpoints = [];
+    for (var i = 0; i < clusters.length; i++) {
+      breakpoints.push(clusters[i][0]);
+    }
+  }
+
+  scale.domain = function () {
+    if (arguments.length) {
+      domain = arguments[0];
+      rescale();
+      return scale;
+    } else {
+      return domain;
+    }
+  };
+
+  scale.range = function () {
+    if (arguments.length) {
+      var newRange = arguments[0];
+      var needsUpdate = newRange.length !== range.length;
+      range = newRange;
+      if (needsUpdate) {
+        rescale();
+      }
+      return scale;
+    } else {
+      return range;
+    }
+  };
+
+  scale.invertExtent = function (rangeValue) {
+    // d3.scaleQuantile.invertExtent returns NaNs for invalid usage, emulate that behavior here
+    var extentA = NaN;
+    var extentB = NaN;
+    for (var i = 0; i < range.length; i++) {
+      if (range[i] === rangeValue) {
+        extentA = breakpoints[i];
+        extentB = i + 1 < range.length ? breakpoints[i + 1] : NaN;
+        break;
+      }
+    }
+    return [extentA, extentB];
+  };
+
+  scale.breaks = function () {
+    return breakpoints.slice(1);
+  };
+
+  scale.copy = function () {
+    return d3scaleCluster().domain(domain).range(range);
+  };
+
+  return scale;
+};
+
