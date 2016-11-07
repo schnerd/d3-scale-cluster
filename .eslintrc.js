84730616d80be02b2493c3700d4d1e7c600ccea5
David Schnurr <schnurr@gmail.com>
2016-11-07 13:58:52 -0800

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "rules": {
+        "semi": ["off"]
+    },
+	"globals": {
+		"d3": true
+	}
+};
